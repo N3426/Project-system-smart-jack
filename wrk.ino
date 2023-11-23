d70544ddbab87fa778b521cd0474f075d484f175
N3428 <81714648+N3426@users.noreply.github.com>
2023-11-23 13:20:26 +0800

    Update wrk.ino

--- wrk.ino
+++ wrk.ino
@@ -5,7 +5,8 @@ const int ledPin1 = 13;  // LED connected to digital pin 13
 const int ledPin2 = 12;
 const int rxPin = 1;   // RXD pin of HC-05 connected to digital pin 10
 const int txPin = 0;   // TXD pin of HC-05 connected to digital pin 11
-const int infraRed = 11; // Infrared sensor
+const int trigPin = 7; // Adjust pin number as needed
+const int echoPin = 8; // Adjust pin number as needed
 const int ldrPin = A0;     // Analog pin for LDR
 // Define the pins for the BTS7960
 const int ENL = 5;  // Left Enable (Speed Control)
@@ -13,13 +14,17 @@ const int ENR = 6;  // Right Enable (Speed Control)
 const int LPWM = 3; // Left PWM (Direction Control)
 const int RPWM = 4; // Right PWM (Direction Control)
 
+const int minDistance = 7; // Minimum distance in cm
+const int maxDistance = 31; // Maximum distance in cm
+
 int ldrValue = 0;          // Variable to store LDR readings
 int threshold = 1000;       // Adjust this threshold value as needed
 // Set up the software serial port
 SoftwareSerial bluetooth(rxPin, txPin);
 
 char Incoming_value = 0;
-char infraRed_value = 0;
+//char infraRed_value = 0;
+char lastCommand = 0; // Variable to store the last command
 bool motorRunning = false; // Flag to keep track of the motor's state
 
 void setup() {
@@ -29,7 +34,8 @@ void setup() {
   pinMode(ledPin2, OUTPUT);
   pinMode(LPWM, OUTPUT);
   pinMode(RPWM, OUTPUT);
-  pinMode(infraRed, INPUT);
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT);  // Sets the echoPin as an Input
   pinMode(ldrPin, INPUT);
   // motor
   pinMode(ENL, OUTPUT);
@@ -45,16 +51,26 @@ void  loop() {
   if (ldrValue > threshold) {
     digitalWrite(ledPin1, LOW); // Turn on the LED
     digitalWrite(ledPin2, HIGH);
+    Serial.println("Dark");
   } else {
     digitalWrite(ledPin1, HIGH);  // Turn off the LED
     digitalWrite(ledPin2, LOW);
+    Serial.println("Light");
   }
 
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2; // Calculate the distance
 
   if (Serial.available() > 0) {
         Incoming_value = Serial.read();
         Serial.print(Incoming_value);
-        Serial.print("\n");
+        Serial.println("\n");
         Serial.println("EN High");
         digitalWrite(ENR, HIGH);
         digitalWrite(ENL, HIGH);
@@ -63,28 +79,26 @@ void  loop() {
 
     // Control logic for '1'
     if (Incoming_value == '1') {
-        // Check if the IR sensor detects an object
-        if (digitalRead(infraRed) == LOW) {
-            Serial.println("Object detected, motor moving clockwise");
+        if (distance > minDistance && distance < maxDistance) {
+            Serial.println("Motor moving clockwise");
             analogWrite(LPWM, 255); // Motor move clockwise
             analogWrite(RPWM, 0);
         } else {
-            Serial.println("No object detected, stopping motor");
+            Serial.println("Object out of range, stopping motor");
             analogWrite(LPWM, 0);
             analogWrite(RPWM, 0);
         }
-    
-    } else if (Incoming_value == '0') {
-            if (digitalRead(infraRed) == HIGH) {
-                Serial.println("Object detected, motor moving anticlockwise");
-                analogWrite(RPWM, 255); // Motor move anticlockwise
-                analogWrite(LPWM, 0);
-            } else {
-                Serial.println("No object detected, stopping motor");
-                analogWrite(RPWM, 0);
-                analogWrite(LPWM, 0);
-            }
+    }
+    // Control logic for '0'
+    else if (Incoming_value == '0') {
+        if (distance > minDistance && distance < maxDistance) {
+            Serial.println("Object within range, motor moving anticlockwise");
+            analogWrite(RPWM, 255); // Motor move anticlockwise
+            analogWrite(LPWM, 0);
+        } else {
+            Serial.println("Object out of range, stopping motor");
+            analogWrite(RPWM, 0);
+            analogWrite(LPWM, 0);
         }
     }
-
-
+}
